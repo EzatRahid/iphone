79742dcfb1b8d85e9b716482da401df0f978d323
EzatRashid <ezat.b.rashid@gmail.com>
2023-10-08 13:58:33 -0600

    phone

---
+++ index.js
@@ -0,0 +1,13 @@
+const password = 1234;
+
+let buttons = document.querySelectorAll('.btn')
+let circle = document.querySelectorAll('.circle')
+
+const keys = [
+    1,2,3,
+    4,5,6,
+    7,8,9,
+      0
+]
+
+console.log(buttons)
